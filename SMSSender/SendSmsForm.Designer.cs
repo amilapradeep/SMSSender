ec6b0d2b4675071e6729335d122d40c406547102
Amila Pradeep <amilapradeep@gmail.com>
2018-04-27 08:39:52 +0400

    initial add

---
+++ SMSSender/SendSmsForm.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace SMSSender
+{
+    partial class SendSmsForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.messageTextTextBox = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.sendButton = new System.Windows.Forms.Button();
+            this.filePathTextBox = new System.Windows.Forms.TextBox();
+            this.browserFileButton = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.totalCharactorsLabel = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // messageTextTextBox
+            // 
+            this.messageTextTextBox.Location = new System.Drawing.Point(251, 24);
+            this.messageTextTextBox.Name = "messageTextTextBox";
+            this.messageTextTextBox.Size = new System.Drawing.Size(820, 202);
+            this.messageTextTextBox.TabIndex = 0;
+            this.messageTextTextBox.Text = "";
+            this.messageTextTextBox.TextChanged += new System.EventHandler(this.messageTextTextBox_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.875F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(33, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(160, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Message Text";
+            // 
+            // sendButton
+            // 
+            this.sendButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.875F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.sendButton.Location = new System.Drawing.Point(494, 365);
+            this.sendButton.Name = "sendButton";
+            this.sendButton.Size = new System.Drawing.Size(187, 63);
+            this.sendButton.TabIndex = 2;
+            this.sendButton.Text = "Send";
+            this.sendButton.UseVisualStyleBackColor = true;
+            this.sendButton.Click += new System.EventHandler(this.sendButton_Click);
+            // 
+            // filePathTextBox
+            // 
+            this.filePathTextBox.Location = new System.Drawing.Point(251, 293);
+            this.filePathTextBox.Name = "filePathTextBox";
+            this.filePathTextBox.ReadOnly = true;
+            this.filePathTextBox.Size = new System.Drawing.Size(666, 31);
+            this.filePathTextBox.TabIndex = 3;
+            // 
+            // browserFileButton
+            // 
+            this.browserFileButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.875F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.browserFileButton.Location = new System.Drawing.Point(953, 293);
+            this.browserFileButton.Name = "browserFileButton";
+            this.browserFileButton.Size = new System.Drawing.Size(118, 39);
+            this.browserFileButton.TabIndex = 4;
+            this.browserFileButton.Text = "Browse";
+            this.browserFileButton.UseVisualStyleBackColor = true;
+            this.browserFileButton.Click += new System.EventHandler(this.browserFileButton_Click);
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.875F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(33, 296);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(212, 89);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Select the Contact List (Excel File)";
+            // 
+            // totalCharactorsLabel
+            // 
+            this.totalCharactorsLabel.AutoSize = true;
+            this.totalCharactorsLabel.Location = new System.Drawing.Point(883, 241);
+            this.totalCharactorsLabel.Name = "totalCharactorsLabel";
+            this.totalCharactorsLabel.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.totalCharactorsLabel.Size = new System.Drawing.Size(193, 25);
+            this.totalCharactorsLabel.TabIndex = 5;
+            this.totalCharactorsLabel.Text = "Character Count: 0";
+            this.totalCharactorsLabel.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.875F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Red;
+            this.label3.Location = new System.Drawing.Point(33, 448);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(1038, 60);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Do not click \'Send\' multiple times. Just click and wait. Application will give yo" +
+    "u message once completed.";
+            // 
+            // SendSmsForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1088, 517);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.totalCharactorsLabel);
+            this.Controls.Add(this.browserFileButton);
+            this.Controls.Add(this.filePathTextBox);
+            this.Controls.Add(this.sendButton);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.messageTextTextBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "SendSmsForm";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Send SMS";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox messageTextTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button sendButton;
+        private System.Windows.Forms.TextBox filePathTextBox;
+        private System.Windows.Forms.Button browserFileButton;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label totalCharactorsLabel;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ SMSSender/SendSmsForm.cs
@@ -0,0 +1,153 @@
+﻿using ExcelDataReader;
+using System;
+using System.Configuration;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SMSSender
+{
+    public partial class SendSmsForm : Form
+    {
+        public string SMSGatewayUrl { get; set; }
+        public string SMSGatewayAuthCode { get; set; }
+        public string MessageText { get; set; }
+        public string LogFilePath { get; set; }
+
+        public SendSmsForm()
+        {
+            InitializeComponent();
+        }
+
+        private void browserFileButton_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            openFileDialog1.Filter = "Excel files (*.xls)|*.xlsx";
+            openFileDialog1.Multiselect = false;
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                string sFileName = openFileDialog1.FileName;
+                filePathTextBox.Text = openFileDialog1.FileName.ToString();
+            }
+            else
+            {
+                filePathTextBox.Text = string.Empty;
+            }
+        }
+
+        private void messageTextTextBox_TextChanged(object sender, EventArgs e)
+        {
+            totalCharactorsLabel.Text = string.Concat("Character Count: ", messageTextTextBox.Text.Count());
+        }
+
+        private void sendButton_Click(object sender, EventArgs e)
+        {
+            SMSGatewayUrl = ConfigurationManager.AppSettings["SMS_GatewayURL"];
+            SMSGatewayAuthCode = ConfigurationManager.AppSettings["SMS_Gateway_AuthCode"];
+
+            if (string.IsNullOrEmpty(filePathTextBox.Text) || string.IsNullOrEmpty(messageTextTextBox.Text)
+                || string.IsNullOrEmpty(SMSGatewayUrl) || string.IsNullOrEmpty(SMSGatewayAuthCode))
+            {
+                MessageBox.Show("Invalid Input", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                //create log file for the current run
+                string folderPath = string.Concat(AppDomain.CurrentDomain.BaseDirectory, "Log\\");
+                if (!Directory.Exists(folderPath))
+                    Directory.CreateDirectory(folderPath);
+
+                LogFilePath = string.Concat(folderPath, DateTime.Now.ToString("yyyy-dd-M HH-mm-ss"), ".txt");
+
+                LogInfo("SMS Sending Started at " + DateTime.Now.ToString("yyyy-dd-M HH-mm-ss"));
+
+                string filePath = filePathTextBox.Text;
+                MessageText = messageTextTextBox.Text;
+
+                SendMessage(filePath);
+            }
+        }
+
+        private async Task SendMessage(string filePath)
+        {
+            FileStream stream = File.Open(filePath, FileMode.Open, FileAccess.Read);
+
+            IExcelDataReader excelReader = ExcelReaderFactory.CreateOpenXmlReader(stream);
+            DataSet dataSet = excelReader.AsDataSet();
+            excelReader.Close();
+
+            int ind = 0;
+
+            for (int i = 0; i < dataSet.Tables.Count; i++)
+            {
+                int row_no = 1;
+                while (row_no < dataSet.Tables[ind].Rows.Count)
+                {
+                    var currRow = dataSet.Tables[ind].Rows[row_no];
+
+                    string phoneNumber = currRow[0].ToString();
+
+                    await SendMessageAsync(phoneNumber, MessageText);
+
+                    row_no++;
+                }
+                ind++;
+            }
+
+            LogInfo("SMS Sending Completed at " + DateTime.Now.ToString("yyyy-dd-M HH-mm-ss"));
+
+            MessageText = string.Empty;
+            filePathTextBox.Text = string.Empty;
+            messageTextTextBox.Text = string.Empty;
+            totalCharactorsLabel.Text = string.Concat("Character Count: ", messageTextTextBox.Text.Count());
+            LogFilePath = string.Empty;
+
+            MessageBox.Show(this, "Sending Completed!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+        }
+
+        private async Task SendMessageAsync(string Phone, string Message)
+        {
+            using (var stringContent = new StringContent("destination=" + Phone + "&q=" + SMSGatewayAuthCode + "&message=" + Message,
+                                                            Encoding.UTF8, "application/x-www-form-urlencoded"))
+            {
+                using (var client = new HttpClient())
+                {
+                    try
+                    {
+                        var response = await client.PostAsync(SMSGatewayUrl, stringContent);
+                        var result = await response.Content.ReadAsStringAsync();
+
+                        if (result == "0")
+                        {
+                            LogInfo(Phone + " Sent at " + DateTime.Now.ToString("yyyy-dd-M HH-mm-ss"));
+                        }
+                        else
+                        {
+                            LogInfo(Phone + " Failed at " + DateTime.Now.ToString("yyyy-dd-M HH-mm-ss") + " " + result);
+                        }
+
+                    }
+                    catch (Exception ex)
+                    {
+                        LogInfo(Phone + " Failed at " + DateTime.Now.ToString("yyyy-dd-M HH-mm-ss") + " - Exception " + ex.Message);
+
+                    }
+                }
+            }
+        }
+
+        private void LogInfo(string logText)
+        {
+            using (StreamWriter sw = new StreamWriter(LogFilePath, true))
+            {
+                sw.WriteLine(logText);
+            }
+        }
+    }
+}
